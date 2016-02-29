07d2f4e33e8eccfc98507b8c8333646bb9900158
Shadowsong27 <Shadowsong@MacBook-Pro-2.local>
2016-02-29 20:28:35 +0800

    Add in loadCompletedTask, loadArchivedTask() and delete() in Storage Class

--- src/Storage.java
+++ src/Storage.java
@@ -16,33 +16,23 @@ public class Storage {
 		return tasks;
 	}
 	
+	public TreeSet<Task> loadCompletedTask(){
+		// read the files, store in data structure
+		TreeSet<Task> tasks = new TreeSet<Task>();
+		return tasks;
+	}
 	
-	reloadCurrentTasks()
-	reloadCompletedTasks()
-	reloadArchivedTasks()
+	public TreeSet<Task> loadArchiveTask(){
+		// read the files, store in data structure
+		TreeSet<Task> tasks = new TreeSet<Task>();
+		return tasks;
+	}
 
-	return a required data structure
-
-	delete(Task obj): delete a task in the text file
-	Input: Task Object
-	Output: Boolean
-
-	getTaskCount(): get the number of tasks stored in the text file
-	getTaskCount(String word) get the number of tasks that contains the search word stored in the text file
-	getTaskCount(Date) :get the number of tasks that related to the input date stored in the text file
-	Output: int
-
-	searchByDate(Date/DateTime)
-	searchByString(String)
-	searchByPriority() : check Tasks attribute
-
-	return a data structure of tasks found
-
-
-	getDirectory()
-	return the directory of the text file in String
-	moveFiles
-
-	clearFile() erases all content in the file
+	public boolean delete(Task obj){
+		// code for delete obj
+		return false;
+	}
+	
+	public 
 
 }

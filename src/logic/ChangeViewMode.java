b35ccc8ccd47e4e272a921ebaf7243a18ae8011d
sexy-monkey <boxin.yang@gmail.com>
2016-04-03 00:24:16 +0800

    collapse all tasks when change view mode

--- src/logic/ChangeViewMode.java
+++ src/logic/ChangeViewMode.java
@@ -1,6 +1,8 @@
 package logic;
 
+import java.util.ArrayList;
 import common.State;
+import common.Task;
 import common.ViewMode;
 
 public class ChangeViewMode implements Operation {
@@ -14,6 +16,13 @@ public class ChangeViewMode implements Operation {
 	public boolean process() {
 		ViewMode newViewMode = state.getNewViewMode();
 		state.setViewMode(newViewMode);
+		
+		//Collape alArrayList<E> when switching view mode
+		ArrayList<Task> allTasks = state.getAllTasks();
+		for(int i = 0; i < allTasks.size(); i++){
+			Task another = allTasks.get(i);
+			another.setIsDetailDisplayed(false);
+		}
 		return true;
 	}
 

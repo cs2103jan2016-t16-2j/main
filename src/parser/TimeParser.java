d2426f81e9e41e5821bee1e0520fd55cb942ec20
MaxKusnadi <max.kusnadi@gmail.com>
2016-04-08 23:15:10 +0800

    Default 2359

--- src/parser/TimeParser.java
+++ src/parser/TimeParser.java
@@ -12,8 +12,21 @@ public class TimeParser{
  
  public static Date stringToDate(String str){
 	 try {
+		 Date current = getCurrentDate();
 		 List<DateGroup> groups = dateParser.parse(str);
-		 return groups.get(0).getDates().get(0);
+		 Date date = groups.get(0).getDates().get(0);
+		 Calendar cal = Calendar.getInstance();
+		 Calendar calCurrent = Calendar.getInstance();
+		 cal.setTime(date);
+		 calCurrent.setTime(current);
+		 if(cal.get(Calendar.HOUR_OF_DAY) == calCurrent.get(Calendar.HOUR_OF_DAY) && cal.get(Calendar.MINUTE) == calCurrent.get(Calendar.MINUTE)){
+			 cal.set(Calendar.HOUR_OF_DAY, 23);
+			 cal.set(Calendar.MINUTE, 59);
+			 cal.set(Calendar.SECOND, 59);
+		 }
+		 
+		 
+		 return cal.getTime();
 	 } catch (Exception e){
 		 return null;
 	 }

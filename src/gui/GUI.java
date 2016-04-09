7cef4e7deab80e7da764741a9e148b4fb65140a4
yuekaidi <kaidi.yue@gmail.com>
2016-04-09 21:11:21 +0800

    update date display

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -3,6 +3,8 @@ package gui;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+
 import common.*;
 import javafx.application.Application;
 import javafx.event.EventHandler;
@@ -52,9 +54,14 @@ public class GUI extends Application{
 	private Label helpHeader;
 	private Label finishedHeader;
 	
-	private SimpleDateFormat sdf = new SimpleDateFormat("dd MMM y HH:mm");
-	private SimpleDateFormat sdfDate = new SimpleDateFormat("dd MMM y");
+	private SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yy HH:mm");
+	private SimpleDateFormat sdfYear = new SimpleDateFormat("yy");
+	private SimpleDateFormat sdfThisYear = new SimpleDateFormat("dd MMM HH:mm");
+	private SimpleDateFormat sdfDate = new SimpleDateFormat("dd MMM \'yy");
+	private SimpleDateFormat sdfDateThisYear = new SimpleDateFormat("dd MMM");
 	private SimpleDateFormat sdfTime = new SimpleDateFormat("HH:mm");
+	private SimpleDateFormat sdfDefaultTime = new SimpleDateFormat("HH:mm:ss");
+	private String defaultTime = "23:59:17";
 	
 	private int taskBoxHeight;
 	private int taskBoxWidth;
@@ -229,14 +236,42 @@ public class GUI extends Application{
 	private String getTaskTime(Task task) {
 		String taskTime = "";
 		if (task.getTaskType().equals(TaskType.DEADLINE)){ 
-			if (task.getStartDate() == null){
+			Date startDate = task.getStartDate();
+			Date endDate = task.getEndDate();
+			boolean sameDate = false;
+			boolean startThisYear = false;
+			boolean hasStartTime = false;
+			boolean endThisYear = sdfYear.format(endDate).equals(sdfYear.format(System.currentTimeMillis()));
+			boolean hasEndTime = sdfDefaultTime.format(endDate).equals(defaultTime);
+			if (startDate != null){
+				startThisYear = sdfYear.format(startDate).equals(sdfYear.format(System.currentTimeMillis()));
+				sameDate = sdfDate.format(task.getStartDate()).equals(sdfDate.format(task.getEndDate()));
+				hasStartTime = sdfDefaultTime.format(startDate).equals(defaultTime);
+			}
+			if (startDate == null && endThisYear && hasEndTime){
+			    taskTime = sdfDateThisYear.format(task.getEndDate());
+			} else if (startDate == null && endThisYear && !hasEndTime){
+			    taskTime = sdfThisYear.format(task.getEndDate());
+			} else if (startDate == null && hasEndTime){
 			    taskTime = sdf.format(task.getEndDate());
-		    } else{
-			    if (sdfDate.format(task.getStartDate()).equals(sdfDate.format(task.getEndDate()))){
-				    taskTime = String.format(DURATION, sdf.format(task.getStartDate()), sdfTime.format(task.getEndDate()));		
-			    } else{
-	    	    	taskTime = String.format(DURATION, sdf.format(task.getStartDate()), sdf.format(task.getEndDate()));				
-		    	}	
+		    } else if (startDate == null && !hasEndTime){
+			    taskTime = sdfDate.format(task.getEndDate());
+		    } else if (sameDate && startThisYear){
+			    taskTime = String.format(DURATION, sdfThisYear.format(task.getStartDate()), sdfTime.format(task.getEndDate()));		
+			} else if (sameDate && !startThisYear){
+	        	taskTime = String.format(DURATION, sdf.format(task.getStartDate()), sdf.format(task.getEndDate()));				
+		    } else if (startThisYear && endThisYear && hasEndTime){
+		    	taskTime = String.format(DURATION, sdfThisYear.format(task.getStartDate()), sdfThisYear.format(task.getEndDate()));
+		    } else if (startThisYear && endThisYear && !hasEndTime){
+		    	taskTime = String.format(DURATION, sdfDateThisYear.format(task.getStartDate()), sdfDateThisYear.format(task.getEndDate()));
+		    } else if (startThisYear && !endThisYear && hasEndTime){
+		    	taskTime = String.format(DURATION, sdfThisYear.format(task.getStartDate()), sdf.format(task.getEndDate()));
+		    } else if (startThisYear && !endThisYear && !hasEndTime){
+		    	taskTime = String.format(DURATION, sdfDateThisYear.format(task.getStartDate()), sdfDate.format(task.getEndDate()));
+		    } else if (!startThisYear && endThisYear && hasEndTime){
+		    	taskTime = String.format(DURATION, sdf.format(task.getStartDate()), sdfThisYear.format(task.getEndDate()));
+		    } else {
+		    	taskTime = String.format(DURATION, sdfDate.format(task.getStartDate()), sdfDateThisYear.format(task.getEndDate()));
 		    }
 		}
 		return taskTime;

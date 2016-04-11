de5c3c7a26b3ad77c8e5c0ae023734c1af3d31f8
sexy-monkey <boxin.yang@gmail.com>
2016-04-11 12:39:57 +0800

    update success message in state j unit testing for add

--- src/logic/Config.java
+++ src/logic/Config.java
@@ -3,20 +3,23 @@ package logic;
 import common.Font;
 import common.State;
 import common.Theme;
+import storage.Storage;
 
 public class Config implements Operation {
 	//============================
 	//       Attributes
 	//============================
 	private State state;
+	private Storage storage;
 
 	
 	//====================================
 	//       Constructor and Initiliser
 	//====================================
 	
-	public Config(State state) {
+	public Config(State state, Storage storage) {
 		this.state = state;
+		this.storage = storage;
 	}
 
 	/**
@@ -40,6 +43,7 @@ public class Config implements Operation {
 		
 		if(newDir != null){
 			state.setNewDirectory(newDir);
+			storage.executeChangeDirectory();
 		}
 		
 		return false;
